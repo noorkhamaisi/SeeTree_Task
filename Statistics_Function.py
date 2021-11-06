142d375ea422a89d92ce324d0099dc006c2a93df
noorkhamaisi <khamaisenoor@gmail.com>
2021-11-06 17:43:42 +0200

    Statistcis_Function and comments

---
+++ Statistics_Function.py
@@ -0,0 +1,53 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 16:26:24 2021
+
+@author: get-a
+"""
+
+import numpy as np               
+from PIL import ImageStat
+
+# https://pillow.readthedocs.io/en/stable/reference/ImageStat.html
+
+##Returnning the MAX value of the image 
+def Max_value(img): 
+    ##This relies on the histogram() method, and simply returns the low and high bins used.
+    min_max = ImageStat.Stat(img).extrema 
+    max_value=min_max[0][1]
+    return max_value
+    
+
+##Returnning the MIN value of the image 
+def Min_value(img): 
+    ##This relies on the histogram() method, and simply returns the low and high bins used.
+    min_max = ImageStat.Stat(img).extrema 
+    min_value=min_max[0][0]
+    return min_value
+ 
+
+
+##Returnning the MEAN of the pixels level in the image 
+def Mean_value(img): 
+    Mean_value = ImageStat.Stat(img).mean[0] 
+    return Mean_value
+
+ 
+ 
+##Returnning the MEDIAN  pixels level in the image
+def Median_value(img): 
+    Median_value = ImageStat.Stat(img).median[0]
+    return Median_value
+
+
+## https://stackoverflow.com/questions/2374640/how-do-i-calculate-percentiles-with-python-numpy
+#Returnning the N'th percentile of the image 
+def percentile_value(img,p):
+    
+    # converting PIL image to Numpy array
+    converttonumpy = np.asarray(img) 
+    # Calculating the  N'th percentile of the image
+    percentile= np.percentile(converttonumpy,p) 
+    return percentile
